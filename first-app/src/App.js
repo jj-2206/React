5bfac9b1e0099b5527d9a7124af9ddbd92edf0c8
jj-2206 <jinju.woo2022@gmail.com>
2022-07-29 00:13:26 +0900

    실습2 App 함수가 리턴하는 값을 그린다!를 항상 의식할 것,,,

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,8 +1,19 @@
-const arr = Array.from(Array(100), (_, i) => i + 1);
-const renderArr = arr.map((item) => <button>{item}</button>);
-
 const App = () => {
-  return <div>{renderArr}</div>;
+  const arr = Array.from(Array(100), (_, i) => i + 1);
+  console.log(arr);
+  return (
+    <div>
+      {arr.map((item, index) => {
+        if (item % 7 === 0) {
+          return <button key={index}>7의 배수</button>;
+        } else if (item % 10 === 0) {
+          return null;
+        } else {
+          return <button key={index}>{item}</button>;
+        }
+      })}
+    </div>
+  );
 };
 
 export default App;
