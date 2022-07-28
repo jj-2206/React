e04c71f7619ff2c6ef49bbe089cd47a2030cc2c8
jj-2206 <jinju.woo2022@gmail.com>
2022-07-29 00:36:20 +0900

    React.StrictMode는 뭘까

--- first-app/src/index.js
+++ first-app/src/index.js
@@ -3,15 +3,16 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.querySelector('#root'));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 // App 컴포넌트 함수를 실행시켜서 반환하는 react element를 가져오기
 
 // root.render(App());
+// root.render(<App />);
 // render함수 = react element를 인자로 받아서 dom element를 그려준다.
-
-// <React.StrictMode>
-//   <App />
-// </React.StrictMode>
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
