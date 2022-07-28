2a8bf657850dd5ef00512b839b70f8aacbbbc74f
jj-2206 <jinju.woo2022@gmail.com>
2022-07-28 23:29:27 +0900

    React Element & React Component React Element: render하기 위한 정보를담아둔 객체 React Component: React Element를 return 하는 함수

--- first-app/src/index.js
+++ first-app/src/index.js
@@ -5,11 +5,15 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
+// App 컴포넌트 함수를 실행시켜서 반환하는 react element를 가져오기
+
+// root.render(App());
+// render함수 = react element를 인자로 받아서 dom element를 그려준다.
+
+// <React.StrictMode>
+//   <App />
+// </React.StrictMode>
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
