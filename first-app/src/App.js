616f290a4ac9cd0f3d6491a372c9bf997574e5fa
jj-2206 <jinju.woo2022@gmail.com>
2022-08-03 02:13:39 +0900

    feat: Answered quiz 1-1

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react';
+// quiz 1-1
+// boolean, null, undefined는 무시됩니다.
+// false, null, undefined와 true는 유효한 자식입니다. 그저 렌더링 되지 않을 뿐입니다. 아래의 JSX 표현식들은 동일하게 렌더링됩니다.
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>{[false, null, undefined, true]}</div>
+      <div>{false}</div>
+      <div>{null}</div>
+      <div>{undefined}</div>
+      <div>{true}</div>
+    </>
   );
-}
+};
 
 export default App;
