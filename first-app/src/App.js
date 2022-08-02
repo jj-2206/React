d8d57a675b312bf4e1bcdf6690f267c5ea04ac00
jj-2206 <jinju.woo2022@gmail.com>
2022-08-03 03:27:32 +0900

    feat: Answered quiz 2-2

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,23 +1,55 @@
-// quiz 2-1
-// Event Handling JSX에서는 onClick, 실행될 함수를 전달
+// quiz 2-2
+// state 컴포넌트 안에서 관리되는 유동적인 데이터
+// [result, setResult] => [value, setter함수]
 
 import React from 'react';
-
-const arr = Array.from(Array(100), (_, i) => i + 1);
 const App = () => {
+  console.log('컴포넌트 재렌더!');
+  const [left, setLeft] = React.useState(0);
+  const [right, setRight] = React.useState(0);
+  const [result, setResult] = React.useState(0);
+
+  const handleChangeLeft = (e) => {
+    setLeft(e.target.value);
+  };
+
+  const handleChangeRight = (e) => {
+    setRight(e.target.value);
+  };
+
+  const handleClickSubmit = () => {
+    setResult(+left + +right);
+  };
+
   return (
-    <div>
-      {arr.map((item) => (
-        <button
-          key={item}
-          onClick={() => {
-            alert(item);
-          }}
-        >
-          {item}
-        </button>
-      ))}
-    </div>
+    <>
+      <input type="number" onChange={handleChangeLeft} />
+      +
+      <input type="number" onChange={handleChangeRight} />
+      =
+      <input type="number" disabled value={result} />
+      <button type="button" onClick={handleClickSubmit}>
+        계산
+      </button>
+    </>
   );
 };
 export default App;
+
+/* 제출코드
+  const [result, setResult] = React.useState(0);
+  const [resultNum1, setNum1] = React.useState(0);
+  const [resultNum2, setNum2] = React.useState(0);
+  return (
+    <>
+      <input type="number" onChange={(e) => setNum1(Number(e.target.value))} />
+      +
+      <input type="number" onChange={(e) => setNum2(Number(e.target.value))} />
+      =
+      <input type="number" disabled value={result} />
+      <button type="button" onClick={() => setResult(resultNum1 + resultNum2)}>
+        계산
+      </button>
+    </>
+  );
+  */
