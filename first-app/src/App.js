1190b02a9b0e11a37d15c9fc6977710f4162ff44
jj-2206 <jinju.woo2022@gmail.com>
2022-08-03 03:35:32 +0900

    feat: Answered quiz 2-3

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,55 +1,22 @@
-// quiz 2-2
-// state 컴포넌트 안에서 관리되는 유동적인 데이터
-// [result, setResult] => [value, setter함수]
+// quiz 2-3
+// useRef
+// ref를 넘겨주면, 해당 dom element 를 current에 담아줌
 
 import React from 'react';
 const App = () => {
-  console.log('컴포넌트 재렌더!');
-  const [left, setLeft] = React.useState(0);
-  const [right, setRight] = React.useState(0);
-  const [result, setResult] = React.useState(0);
-
-  const handleChangeLeft = (e) => {
-    setLeft(e.target.value);
+  const input = React.useRef(null);
+  const handleClick = () => {
+    if (input.current) {
+      input.current.value = '';
+    }
   };
-
-  const handleChangeRight = (e) => {
-    setRight(e.target.value);
-  };
-
-  const handleClickSubmit = () => {
-    setResult(+left + +right);
-  };
-
   return (
-    <>
-      <input type="number" onChange={handleChangeLeft} />
-      +
-      <input type="number" onChange={handleChangeRight} />
-      =
-      <input type="number" disabled value={result} />
-      <button type="button" onClick={handleClickSubmit}>
-        계산
+    <div>
+      <input type="text" ref={input} />
+      <button type="button" onClick={handleClick}>
+        Click to Reset
       </button>
-    </>
+    </div>
   );
 };
 export default App;
-
-/* 제출코드
-  const [result, setResult] = React.useState(0);
-  const [resultNum1, setNum1] = React.useState(0);
-  const [resultNum2, setNum2] = React.useState(0);
-  return (
-    <>
-      <input type="number" onChange={(e) => setNum1(Number(e.target.value))} />
-      +
-      <input type="number" onChange={(e) => setNum2(Number(e.target.value))} />
-      =
-      <input type="number" disabled value={result} />
-      <button type="button" onClick={() => setResult(resultNum1 + resultNum2)}>
-        계산
-      </button>
-    </>
-  );
-  */
