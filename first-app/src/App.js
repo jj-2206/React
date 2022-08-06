cb2adcc1b03fe1763eaebeb62d209db3522161a8
jj-2206 <jinju.woo2022@gmail.com>
2022-08-06 19:10:07 +0900

    feat: Answered quiz 3-2

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,26 +1,64 @@
-// quiz 3-1
-// useRef
-// ref는 dom을 담을 때만 쓸 수 있다? -> X
-// ref는 값이 바뀌어도 컴포넌트가 re-render 되지 않는다? -> O
-// react에 의한 re-render가 아닌, 실제 document에 존재하는 element를 직접 접근하여 수정
-// react state로는 관리할 수 없는 경우에만 사용하는 것이 적절
-
-import React from 'react';
+import React, { useState, useRef } from 'react';
 
 const App = () => {
-  const input = React.useRef(null);
-  const [value, setValue] = React.useState('');
-  const handleClick = () => {
-    setValue('');
-    if (input.current) {
-      input.current.focus();
+  const [id, setId] = useState('');
+  const [pw, setPw] = useState('');
+
+  const idRef = useRef(null);
+  const pwRef = useRef(null);
+
+  const idError = '유효하지 않은 id입니다.';
+  const pwError = '유효하지 않은 password입니다.';
+
+  const idCheck = id.length >= 6 && id.length <= 20;
+  const pwCheck = pw.length >= 12 && pw.length <= 20;
+
+  const handleChange = (event) => {
+    if (event.target.name === 'id') {
+      setId(event.target.value);
     }
+    if (event.target.name === 'pw') {
+      setPw(event.target.value);
+    }
+  };
+
+  const handleClick = () => {
+    if (!idCheck) {
+      alert(idError);
+      setId('');
+      idRef.current.focus();
+    } else if (!pwCheck) {
+      alert(pwError);
+      setPw('');
+      pwRef.current.focus();
+    } else alert('회원가입 성공!');
   };
   return (
     <div>
-      <input type="text" ref={input} value={value} onChange={(e) => setValue(e.target.value)} />
-      <button type="button" onClick={handleClick}>
-        Click to Reset and Focus!
+      <div>
+        <input
+          type="text"
+          name="id"
+          ref={idRef}
+          value={id}
+          onChange={handleChange}
+          placeholder="6글자 이상 20글자 이하"
+        />
+        <div>{idCheck ? null : idError}</div>
+      </div>
+      <div>
+        <input
+          type="text"
+          name="pw"
+          ref={pwRef}
+          value={pw}
+          onChange={handleChange}
+          placeholder="12글자 이상 20글자 이하"
+        />
+        <div>{pwCheck ? null : pwError}</div>
+      </div>
+      <button type="button" onClick={handleClick} disabled={!(id || pw)}>
+        회원가입
       </button>
     </div>
   );
