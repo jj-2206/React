9ad14bb2768450fff43aaae85ff11d9eb0e679fb
jj-2206 <jinju.woo2022@gmail.com>
2022-08-07 03:18:48 +0900

    feat: Create answer quiz 5-1

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,44 +1,148 @@
-// 5-0
-// react-router react-router란?
-// => react 프로젝트에서 SPA를 쉽게 도입할 수 있도록 도와주는 라이브러리
-// parameter
-
+// 5-1
+import React from 'react';
 import {
   BrowserRouter,
+  useParams,
   Route,
   Routes,
   Link,
-  useParams,
 } from 'react-router-dom';
 
 const Hello = () => {
   const params = useParams();
-  console.log(params); // {id: '5', id2: '9'}
+  console.log('params객체: ', params);
   return (
-    <Link to="/">
-      to home {params.id} {params.id2}
-    </Link>
+    <>
+      this is Hello Component <br />
+      Hello {params.id}!
+      <br />
+      <Link to="/">return to form </Link>
+    </>
   );
 };
-// <Link/>
-// props.to 에 적힌 url로 http get request를 보내지 않고 브라우저에 표시되는 url만 변경
-
-const Main = () => <Link to="/hello/5">to hello with id 5</Link>;
 
 const App = () => {
   return (
     <BrowserRouter>
-      {/* <BrowserRouter/> react-router를 사용할 앱에서 단 한번만 사용 */}
       <Routes>
-        {/*  <Routes/> <Route />를 아래와 같이 동작하도록 만듦 */}
-        <Route path="/hello/:id/:id2" element={<Hello />} />
-
-        <Route path="/" element={<Main />} />
-        {/* <Route/> props.path와 브라우저의 url이 일치하면 props.element를 return 
-        가장 먼저 일치하는 Route 만 적용 */}
+        {' '}
+        {/* swtich(주소창 내용물) */}
+        {/* case: 주소창이 "/" 면 <Form /> 렌더; break; */}
+        <Route path="/" element={<Form />} />
+        {/* case: 주소창이 "/hello/아무문자열" 이면 <Hello /> 렌더; break; */}
+        <Route path="/hello/:id" element={<Hello />} />
       </Routes>
     </BrowserRouter>
   );
 };
 
+const fields = [
+  {
+    key: 'id',
+    label: 'id',
+    placeholder: '6글자 이상 20글자 이하',
+    initialValue: '',
+    checkValid: (v) => v.length >= 6 && v.length <= 20,
+  },
+  {
+    key: 'pw',
+    label: 'password',
+    placeholder: '12글자 이상 20글자 이하',
+    initialValue: '',
+    checkValid: (v) => v.length >= 12 && v.length <= 20,
+  },
+  {
+    key: 'name',
+    label: '이름',
+    initialValue: '',
+    placeholder: '아무이름',
+    checkValid: (v) => v.length >= 1 && v.length <= 5,
+  },
+];
+
+const Form = () => {
+  const [values, setValues] = React.useState(
+    fields.reduce(
+      (acc, { key, initialValue }) => ({ ...acc, [key]: initialValue }),
+      {}
+    )
+  );
+  const refs = React.useRef(Array.from(Array(fields.length, () => null)));
+  const valids = fields.map(({ key, checkValid }) => checkValid(values[key]));
+  const isButtonDisabled = !Object.values(values).some((value) => value.length);
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    // 클릭하면 이동하는 기능 막기
+
+    const isAllValid = valids.every((isValid, i) => {
+      const { key, initialValue, label } = fields[i];
+      if (!isValid) {
+        setValues((prev) => ({
+          ...prev,
+          [key]: initialValue,
+        }));
+        refs.current[i].focus();
+        alert(`유효하지 않은 ${label}입니다`);
+      }
+      return isValid;
+    });
+
+    if (!isAllValid) {
+      return;
+    }
+    alert('회원가입 성공!');
+  };
+
+  const handleChangeValue = (e) => {
+    setValues((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const isAllValid = valids.every((a) => a);
+  // 각각의 valid 유효성을 검사하는 변수
+
+  return (
+    <div>
+      {fields.map(({ key, label, placeholder }, i) => (
+        <div key={key}>
+          <input
+            type="text"
+            ref={(ref) => (refs.current[i] = ref)}
+            name={key}
+            value={values[key]}
+            placeholder={placeholder}
+            onChange={handleChangeValue}
+          />
+          {!valids[i] && `유효하지 않은 ${label}입니다`}
+        </div>
+      ))}
+
+      <Link to={`/hello/${values.id}`}>
+        <button
+          type="button"
+          onClick={isAllValid ? undefined : handleClick}
+          disabled={isButtonDisabled}
+        >
+          회원가입
+        </button>
+      </Link>
+      {/* 조건부 렌더링이 아닌, 페이지 이동을 하는 기본 동작을 막는 방법 */}
+
+      {isAllValid ? (
+        <Link to={`/hello/${values.id}`}>
+          <button type="button">(유효한버튼) 회원가입</button>
+        </Link>
+      ) : (
+        <button type="button" onClick={handleClick} disabled={isButtonDisabled}>
+          (유효하지 않은 버튼) 회원가입
+        </button>
+      )}
+
+      {/* 조건부 렌더링으로 유효할 때만 Link로 다른 페이지 이동이 가능하게 만드는 방법 */}
+    </div>
+  );
+};
 export default App;
