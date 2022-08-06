b471f5b633f2d43409e9477e752ab04de608e68e
jj-2206 <jinju.woo2022@gmail.com>
2022-08-07 00:41:19 +0900

    fix: Add using useState useEffect의 cleanup 함수 쓰기 (좋지 않은 예시)

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,32 +1,34 @@
-import React, { useEffect, useRef } from 'react';
+// quiz 4-1
 
-// useRef로 DOM에 직접 접근, 지양해야하는 것 중 하나.
+// useRef 없이 useState의 setter함수로 최신 value를 가져와서 출력할 수도 있습니다.
+// 지양합시다 패턴. -> useEffect의 클린업 함수를 사용하는 것이 좋습니다.
 
+import React, { useEffect } from 'react';
 const App = () => {
   const [value, setValue] = React.useState('');
-  const ref = React.useRef();
 
   useEffect(
     () => {
       const onClick = () => {
-        console.log(`현재 value는 ${ref.current.value}입니다!`);
+        setValue((prev) => {
+          console.log(`현재 value는 ${prev} 입니다!`);
+          return prev;
+        });
       };
       document.addEventListener('click', onClick);
     },
     [
-      // value
+      //value
     ]
   );
+
   return (
-    <>
-      <input
-        ref={ref}
-        value={value}
-        onChange={(e) => {
-          setValue(e.target.value);
-        }}
-      />
-    </>
+    <input
+      value={value}
+      onChange={(e) => {
+        setValue(e.target.value);
+      }}
+    />
   );
 };
 export default App;
