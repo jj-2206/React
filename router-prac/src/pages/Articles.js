d6e8b72c3c9c4c12bfb370307bacbd1687feda8e
jj-2206 <jinju.woo2022@gmail.com>
2022-08-07 17:12:18 +0900

    fix: Add using NavLink

--- router-prac/src/pages/Articles.js
+++ router-prac/src/pages/Articles.js
@@ -1,35 +1,69 @@
-import { Link, Outlet, useNavigate } from 'react-router-dom';
+import { Link, Outlet, useNavigate, NavLink } from 'react-router-dom';
 
 const Articles = () => {
   const navigate = useNavigate();
 
-  const goBack = () => {
-    // 이전 페이지로 이동
-    navigate(-1);
-  };
-
   const goAbout = () => {
     navigate('/about');
+  };
+
+  const ArticleItems = ({ id }) => {
+    const activeStyle = {
+      color: 'green',
+      fontSize: 20,
+    };
+    return (
+      <li>
+        <NavLink
+          to={`/articles/${id}`}
+          style={({ isActive }) => (isActive ? activeStyle : undefined)}
+        >
+          게시글 {id}
+        </NavLink>
+      </li>
+    );
   };
 
   return (
     <div>
       <Outlet />
       <ul>
-        <li>
-          <Link to="/articles/1">게시글 1</Link>
-        </li>
-        <li>
-          <Link to="/articles/2">게시글 2</Link>
-        </li>
-        <li>
-          <Link to="/articles/3">게시글 3</Link>
-        </li>
+        <ArticleItems id={1} />
+        <ArticleItems id={2} />
+        <ArticleItems id={3} />
       </ul>
-      <button onClick={goBack}> 뒤로 가기 </button>
       <button onClick={goAbout}> 소개로 가기 </button>
     </div>
   );
 };
 
 export default Articles;
+
+{
+  /* <ul>
+<li>
+  <NavLink
+    to="/articles/1"
+    style={({ isActive }) => (isActive ? activeStyle : undefined)}
+  >
+    게시글 1
+  </NavLink>
+</li>
+<li>
+  <NavLink
+    to="/articles/2"
+    style={({ isActive }) => (isActive ? activeStyle : undefined)}
+  >
+    게시글 2
+  </NavLink>
+</li>
+<li>
+  <NavLink
+    to="/articles/3"
+    style={({ isActive }) => (isActive ? activeStyle : undefined)}
+  >
+    게시글 3
+  </NavLink>
+</li>
+</ul> */
+}
