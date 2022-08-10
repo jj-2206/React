3b0061bd5072ceeb06933d2bceef17e433624e24
jj-2206 <jinju.woo2022@gmail.com>
2022-08-11 01:10:35 +0900

    fix: Add Using classnames

--- hello-react/src/CSSModule.js
+++ hello-react/src/CSSModule.js
@@ -6,6 +6,7 @@ const cnBind = classNames.bind(styles);
 const CSSModule = () => {
   return (
     <div className={cnBind('wrapper', 'inverted')}>
+      {/* <div className={`${styles.wrapper} ${styles.inverted}`}> */}
       안녕하세요. 저는 <span className="something">CSS Module!</span>
     </div>
   );
