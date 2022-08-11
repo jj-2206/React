b10f6796fd89238b43c5b0ca1cc903328d303709
jj-2206 <jinju.woo2022@gmail.com>
2022-08-11 14:59:27 +0900

    fix: Using useContext Hook
    Context.Consumer Render Props 방식에서 useContext Hook으로 코드변경.

--- hello-react-2/src/components/ColorBox.js
+++ hello-react-2/src/components/ColorBox.js
@@ -1,30 +1,27 @@
+import { useContext } from 'react';
 import ColorContext from '../contexts/color';
 
-// Consumer: Context의 value를 소비(조회) (props로 받아오는 것이 아니다!)
-// Render Props 컴포넌트의 children이 있어야할 자리에 함수를 전달!
+// useContext Hook 사용
 
 const ColorBox = () => {
+  const { state } = useContext(ColorContext);
   return (
-    <ColorContext.Consumer>
-      {({ state }) => (
-        <>
-          <div
-            style={{
-              width: '100px',
-              height: '100px',
-              background: state.color,
-            }}
-          />
-          <div
-            style={{
-              width: '50px',
-              height: '50px',
-              background: state.subcolor,
-            }}
-          />
-        </>
-      )}
-    </ColorContext.Consumer>
+    <>
+      <div
+        style={{
+          width: '100px',
+          height: '100px',
+          background: state.color,
+        }}
+      />
+      <div
+        style={{
+          width: '50px',
+          height: '50px',
+          background: state.subcolor,
+        }}
+      />
+    </>
   );
 };
 
