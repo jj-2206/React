2db29c9e5a6db86f8b999883f00e2e354c410291
jj-2206 <jinju.woo2022@gmail.com>
2022-08-12 05:07:36 +0900

    fix: Using useReducer ch11

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,4 +1,4 @@
-import { useState, useRef, useCallback } from 'react';
+import { useReducer, useRef, useCallback } from 'react';
 import TodoInsert from './todo-components/TodoInsert';
 import TodoList from './todo-components/TodoList';
 import TodoTemplates from './todo-components/TodoTemplates';
@@ -16,13 +16,30 @@ const createBulkTodos = () => {
   return array;
 };
 
+const todoReducer = (todos, action) => {
+  switch (action.type) {
+    case 'INSERT':
+      return todos.concat(action.todo);
+    case 'REMOVE':
+      return todos.filter((todo) => todo.id !== action.id);
+    case 'TOGGLE':
+      return todos.map((todo) =>
+        todo.id === action.id ? { ...todo, checked: !todo.checked } : todo,
+      );
+    default:
+      return todos;
+  }
+};
+
 const App = () => {
-  const [todos, setTodos] = useState(createBulkTodos);
+  const [todos, dispatch] = useReducer(todoReducer, undefined, createBulkTodos);
+  // const [state, dispatch] = useReducer(reducer, initialArg, init);
 
   // 고윳값으로 사용될 id
   // ref를 사용하여 변수 담기
   const nextId = useRef(2501);
 
+  // props로 함수를 전달할 때에는 useCallback을 사용해서 전달하기
   const onInsert = useCallback((text) => {
     const todo = {
       id: nextId.current,
@@ -30,13 +47,13 @@ const App = () => {
       checked: false,
     };
     // useState의 함수형 업데이트는 dependency array를 빈 배열로 만들 수 있다.
-    setTodos((todos) => todos.concat(todo));
+    dispatch({ type: 'INSERT', todo });
     nextId.current += 1; // nextId 1씩 더하기
   }, []);
 
   // 삭제 기능: 같은 id인 todo를 배열에서 삭제
   const onRemove = useCallback((id) => {
-    setTodos((todos) => todos.filter((todo) => todo.id !== id));
+    dispatch({ type: 'REMOVE', id });
   }, []);
 
   // 수정 기능
@@ -44,12 +61,7 @@ const App = () => {
   // ontoggle = (event) => { };
 
   const onToggle = useCallback((id) => {
-    setTodos((todos) =>
-      todos.map(
-        (todo) => (todo.id === id ? { ...todo, checked: !todo.checked } : todo),
-        // todo의 checked를 반대로 하는 객체를 생성하거나 그대로 반환
-      ),
-    );
+    dispatch({ type: 'TOGGLE', id });
   }, []);
 
   return (
