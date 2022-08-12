11430841c39a22efc343e7c20db6b2b834e2dd29
jj-2206 <jinju.woo2022@gmail.com>
2022-08-12 23:56:52 +0900

    fix: Add about prop

--- saenghwal/src/App.js
+++ saenghwal/src/App.js
@@ -1,49 +1,54 @@
 import './App.css';
 
 // 컴포넌트!
-function Header() {
+// 속성 prop
+function Header(props) {
   return (
     <header>
       <h1>
-        <a href="/">REACT</a>
+        <a href="/">{props.title}</a>
       </h1>
     </header>
   );
 }
 
-const Nav = () => {
+const Nav = (props) => {
+  const lis = [];
+  for (let i = 0; i < props.topics.length; i++) {
+    let t = props.topics[i];
+    lis.push(
+      <li key={t.id}>
+        <a href={'/read/' + t.id}>{t.title}</a>
+      </li>
+    );
+  }
   return (
     <nav>
-      <ol>
-        <li>
-          <a href="/read/1">html</a>
-        </li>
-        <li>
-          <a href="/read/2">css</a>
-        </li>
-        <li>
-          <a href="/read/3">js</a>
-        </li>
-      </ol>
+      <ol>{lis}</ol>
     </nav>
   );
 };
 
-const Article = () => {
+const Article = (props) => {
   return (
     <article>
-      <h2>Welcome</h2>
-      Hello, WEB
+      <h2>{props.title}</h2>
+      {props.body}
     </article>
   );
 };
 
 function App() {
+  const topics = [
+    { id: 1, title: 'html', body: 'html is...' },
+    { id: 2, title: 'css', body: 'css is...' },
+    { id: 3, title: 'js', body: 'js is...' },
+  ];
   return (
     <div>
-      <Header></Header>
-      <Nav></Nav>
-      <Article></Article>
+      <Header title="REACT"></Header>
+      <Nav topics={topics}></Nav>
+      <Article title="Welcome" body="Hello, WEB"></Article>
     </div>
   );
 }
