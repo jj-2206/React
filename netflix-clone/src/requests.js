d16d7b21e2cf26a0d24978f1354e6142e76b2f62
jj-2206 <jinju.woo2022@gmail.com>
2022-08-27 02:50:03 +0900

    fix: Protect APIKEY

--- netflix-clone/src/requests.js
+++ netflix-clone/src/requests.js
@@ -1,8 +1,8 @@
-const API_KEY = "YOUR_API_KEY";
+import { API_KEY } from "./apikey";
 
 const requests = {
   fetchTrending: `/trending/all/week?api_key=${API_KEY}`,
-  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
   fetchTopRated: `/movie/top_rated?api_key=${API_KEY}`,
   fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
   fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
