f63345d999c297c67f264e7651414b5465de3eb0
jj-2206 <jinju.woo2022@gmail.com>
2022-08-27 04:13:43 +0900

    feat: Create Main page with data

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,7 +1,23 @@
+import React from "react";
+import Row from "./Row.js";
+import requests from "./requests";
+import "./App.css";
+
 function App() {
   return (
-    <div className="App">
-      <h1>clone coding using axios</h1>
+    <div className="app">
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow={true}
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Top Rated TV" fetchUrl={requests.fetchTopRatedTv} />
+      <Row title="Drama Movies" fetchUrl={requests.fetchDramaMovies} />
+      <Row title="Crime Movies" fetchUrl={requests.fetchCrimeMovies} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Mystery Movies" fetchUrl={requests.fetchMysteryMovies} />
     </div>
   );
 }
