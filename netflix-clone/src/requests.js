509bbd85aa1cf9a6ee758505542ae163d42e18aa
jj-2206 <jinju.woo2022@gmail.com>
2022-08-27 04:21:55 +0900

    feat: Create Main page with data

--- netflix-clone/src/requests.js
+++ netflix-clone/src/requests.js
@@ -1,16 +1,17 @@
-import { API_KEY } from "./apikey";
+import { API_KEY } from "./apikey.js";
 
 const requests = {
-  fetchTrending: `/trending/all/week?api_key=${API_KEY}`,
   fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}`,
   fetchTopRated: `/movie/top_rated?api_key=${API_KEY}`,
+  fetchTopRatedTv: `/tv/top_rated?api_key=${API_KEY}`,
+  fetchDramaMovies: `/discover/movie?api_key=${API_KEY}&with_genres=18`,
+  fetchCrimeMovies: `/discover/movie?api_key=${API_KEY}&with_genres=80`,
   fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
-  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
-  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
-  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
-  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+  fetchMysteryMovies: `/discover/movie?api_key=${API_KEY}&with_genres=9648`,
 };
 
 export default requests;
 
 // 커다란 자료형
+// &language=en-US
