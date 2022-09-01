49c0ae57aafced59701569f890485fa945908542
jj-2206 <jinju.woo2022@gmail.com>
2022-09-01 16:49:40 +0900

    feat: Test UseDispatch
    UseDispatch로 action(addMovies) dispatch해오기

--- prac-redux-toolkit/src/components/Home/Home.js
+++ prac-redux-toolkit/src/components/Home/Home.js
@@ -2,11 +2,15 @@ import React, { useEffect } from "react";
 import MovieListing from "../MovieListing/MovieListing";
 import movieApi from "../../common/apis/movieApi";
 import { API_KEY } from "../../common/apis/MovieApiKey";
+import { useDispatch } from "react-redux";
+import { addMovies } from "../../features/movies/movieSlice";
 
 const Home = () => {
+  const movieText = "Harry";
+  const page = 1;
+  const dispatch = useDispatch();
+
   useEffect(() => {
-    const movieText = "Harry";
-    const page = 1;
     const fetchMovies = async () => {
       const response = await movieApi
         .get(`?apikey=${API_KEY}&s=${movieText}&type=movie&page=${page}`)
@@ -14,6 +18,7 @@ const Home = () => {
           console.log("Err :", err);
         });
       console.log(response);
+      dispatch(addMovies(response.data));
     };
     fetchMovies();
   }, []);
