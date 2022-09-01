614f2b4b2c91eb4c611351b8f570f0bfd6f12888
jj-2206 <jinju.woo2022@gmail.com>
2022-09-01 15:46:20 +0900

    feat: Create prac-redux-toolkit project
    디렉터리 구조
    Header Footer 스타일링 scss 구글 폰트 사용

---
+++ prac-redux-toolkit/src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Footer.scss";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div>Movie App</div>
+      <div>©2022, Dipesh Malvia YouTube jj-2022</div>
+    </div>
+  );
+}
+
+export default Footer;
