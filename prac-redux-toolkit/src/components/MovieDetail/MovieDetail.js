286d19b32aa93dffbf1790aef6a122ca755856fe
jj-2206 <jinju.woo2022@gmail.com>
2022-09-02 02:43:47 +0900

    fix: Add loading...
    데이터가 받아와지는 동안 loading... 보이게 하기

--- prac-redux-toolkit/src/components/MovieDetail/MovieDetail.js
+++ prac-redux-toolkit/src/components/MovieDetail/MovieDetail.js
@@ -13,7 +13,7 @@ function MovieDetail() {
   const { imdbID } = useParams();
   const dispatch = useDispatch();
   const data = useSelector(getSelectedMovieOrShow);
-  console.log(data);
+  // console.log(data);
   useEffect(() => {
     dispatch(fetchAsyncMovieOrShowDetail(imdbID));
     return () => {
@@ -22,49 +22,55 @@ function MovieDetail() {
   }, [dispatch, imdbID]);
   return (
     <div className="movie-section">
-      <div className="section-left">
-        <div className="movie-title">{data.Title}</div>
-        <div className="movie-rating">
-          <span>
-            IMDB Rating <FaStar color="#ff9e00" /> : {data.imdbRating}
-          </span>
-          <span>
-            IMDB Votes <FaThumbsUp color="#fafafa" /> : {data.imdbVotes}
-          </span>
-          <span>
-            Runtime <FaFilm color="rgb(191,213,214)" /> : {data.Runtime}
-          </span>
-          <span>
-            Year <FaCalendarAlt color="peachpuff" /> : {data.Year}
-          </span>
-        </div>
-        <div className="movie-plot">{data.Plot}</div>
-        <div className="movie-info">
-          <div>
-            <span>Director</span>
-            <span>{data.Director}</span>
+      {Object.keys(data).length === 0 ? (
+        <div>...loading</div>
+      ) : (
+        <>
+          <div className="section-left">
+            <div className="movie-title">{data.Title}</div>
+            <div className="movie-rating">
+              <span>
+                IMDB Rating <FaStar color="#ff9e00" /> : {data.imdbRating}
+              </span>
+              <span>
+                IMDB Votes <FaThumbsUp color="#fafafa" /> : {data.imdbVotes}
+              </span>
+              <span>
+                Runtime <FaFilm color="rgb(191,213,214)" /> : {data.Runtime}
+              </span>
+              <span>
+                Year <FaCalendarAlt color="peachpuff" /> : {data.Year}
+              </span>
+            </div>
+            <div className="movie-plot">{data.Plot}</div>
+            <div className="movie-info">
+              <div>
+                <span>Director</span>
+                <span>{data.Director}</span>
+              </div>
+              <div>
+                <span>Stars</span>
+                <span>{data.Actors}</span>
+              </div>
+              <div>
+                <span>Genres</span>
+                <span>{data.Genre}</span>
+              </div>
+              <div>
+                <span>Languages</span>
+                <span>{data.Language}</span>
+              </div>
+              <div>
+                <span>Awards</span>
+                <span>{data.Awards}</span>
+              </div>
+            </div>
           </div>
-          <div>
-            <span>Stars</span>
-            <span>{data.Actors}</span>
+          <div className="section-right">
+            <img src={data.Poster} alt={data.Title} />
           </div>
-          <div>
-            <span>Genres</span>
-            <span>{data.Genre}</span>
-          </div>
-          <div>
-            <span>Languages</span>
-            <span>{data.Language}</span>
-          </div>
-          <div>
-            <span>Awards</span>
-            <span>{data.Awards}</span>
-          </div>
-        </div>
-      </div>
-      <div className="section-right">
-        <img src={data.Poster} alt={data.Title} />
-      </div>
+        </>
+      )}
     </div>
   );
 }
