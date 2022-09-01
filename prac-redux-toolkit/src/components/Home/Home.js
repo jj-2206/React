8a4c80ddc1536941c335582be8e711164ee520fe
jj-2206 <jinju.woo2022@gmail.com>
2022-09-01 16:05:55 +0900

    feat: Call API
    infinite scroll 연습을 위해 page 추가
    axios.create로 baseURL 설정
    API를 한번만 불러올 수 있도록 useEffect의 디펜던시 어레이는 공란으로 설정
    movieApi(baseURL)컴포넌트로 .get() API 통신

--- prac-redux-toolkit/src/components/Home/Home.js
+++ prac-redux-toolkit/src/components/Home/Home.js
@@ -1,5 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
+import MovieListing from "../MovieListing/MovieListing";
+import movieApi from "../../common/apis/movieApi";
+import { API_KEY } from "../../common/apis/MovieApiKey";
 
-export default function Home() {
-  return <div>Home</div>;
-}
+const Home = () => {
+  useEffect(() => {
+    const movieText = "Harry";
+    const page = 1;
+    const fetchMovies = async () => {
+      const response = await movieApi
+        .get(`?apikey=${API_KEY}&s=${movieText}&type=movie&page=${page}`)
+        .catch((err) => {
+          console.log("Err :", err);
+        });
+      console.log(response);
+    };
+    fetchMovies();
+  }, []);
+
+  return (
+    <div>
+      <div className="banner-img"></div>
+      <MovieListing />
+    </div>
+  );
+};
+
+export default Home;
